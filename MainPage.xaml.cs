9248dfa875f3d341a95374982f6efabee9cec0e3
Quilixy <yldryd159@gmail.com>
2025-05-15 19:46:17 +0300

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpChatting;
+
+public partial class MainPage : ContentPage
+{
+    private UdpClient udpClient;
+    private int localPort = 11000;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        StartListening();
+        string keyword = "GİZLİANAHTAR";
+        var playfair = new PlayfairCipher(keyword);
+        playfair.PrintMatrix();
+    }
+
+    private async void OnSendClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            string ipAddress = ipEntry.Text;
+            string username = usernameEntry.Text;
+            string message = messageEntry.Text;
+            string encryptedMessage = PlayfairCipher.Encrypt(message);
+            string fullMessage = $"{username}:{encryptedMessage}";
+
+            if (string.IsNullOrWhiteSpace(ipAddress) || string.IsNullOrWhiteSpace(fullMessage))
+                return;
+
+            var remoteEndpoint = new IPEndPoint(IPAddress.Parse(ipAddress), localPort);
+            byte[] sendBytes = Encoding.UTF8.GetBytes(fullMessage);
+
+            using (UdpClient senderClient = new UdpClient())
+            {
+                await senderClient.SendAsync(sendBytes, sendBytes.Length, remoteEndpoint);
+            }
+
+            messagesLabel.Text += $"[Gönderildi -> {ipAddress}] {message}\n";
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Hata", ex.Message, "Tamam");
+        }
+    }
+
+    private async void StartListening()
+    {
+        try
+        {
+            udpClient = new UdpClient(localPort);
+
+            while (true)
+            {
+                var result = await udpClient.ReceiveAsync();
+                string receivedFullMessage = Encoding.UTF8.GetString(result.Buffer);
+                string[] splittedMessage = receivedFullMessage.Split(':', 2);
+                string senderUserName = splittedMessage.Length > 1 ? splittedMessage[0] : "Bilinmiyor";
+                string receivedCryptedMessage = splittedMessage.Length > 1 ? splittedMessage[1] : "Bilinmiyor";
+                string receivedMessage = PlayfairCipher.Decrypt(receivedCryptedMessage);
+                string senderIp = result.RemoteEndPoint.Address.ToString();
+                
+
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    messagesLabel.Text += $"[Geldi <- {senderUserName}:({senderIp})] {receivedMessage}\n";
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Hata", ex.Message, "Tamam");
+        }
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        udpClient?.Close();
+    }
+}
---
+++ PlayfairCipher.cs
@@ -0,0 +1,150 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace UdpChatting;
+class PlayfairCipher
+{
+    private static readonly char[] Alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZXWQ().,".ToCharArray();
+    private const int Size = 6;
+    private static char[,] matrix = new char[Size, Size];
+
+    public PlayfairCipher(string keyword)
+    {
+        GenerateMatrix(keyword);
+    }
+
+    private void GenerateMatrix(string keyword)
+    {
+        var cleanKeyword = new string(keyword
+            .ToUpper()
+            .Where(c => Alphabet.Contains(c))
+            .Distinct()
+            .ToArray());
+
+        var fullKey = cleanKeyword + new string(Alphabet.Where(c => !cleanKeyword.Contains(c)).ToArray());
+
+        for (int i = 0; i < Size * Size; i++)
+        {
+            matrix[i / Size, i % Size] = fullKey[i];
+        }
+    }
+
+    private static (int row, int col) FindPosition(char ch)
+    {
+        for (int row = 0; row < Size; row++)
+            for (int col = 0; col < Size; col++)
+                if (matrix[row, col] == ch)
+                    return (row, col);
+        throw new Exception("Harf matris içinde bulunamadı: " + ch);
+    }
+
+    private static List<(char, char)> PrepareDigraphs(string text)
+    {
+        List<(char, char)> digraphs = new();
+        var cleanText = new string(text.ToUpper().Where(c => Alphabet.Contains(c)).ToArray());
+
+        for (int i = 0; i < cleanText.Length; i++)
+        {
+            char a = cleanText[i];
+            char b = (i + 1 < cleanText.Length) ? cleanText[i + 1] : 'X';
+
+            if (a == b)
+            {
+                digraphs.Add((a, 'X'));
+            }
+            else
+            {
+                digraphs.Add((a, b));
+                i++; // İkili harf kullandık, i'yi artır
+            }
+        }
+
+        if (digraphs.Last().Item2 == 'X' && cleanText.Length % 2 == 1)
+        {
+            digraphs.Add((cleanText.Last(), 'X'));
+        }
+
+        return digraphs;
+    }
+
+    public static string Encrypt(string plaintext)
+    {
+        var digraphs = PrepareDigraphs(plaintext);
+        StringBuilder sb = new();
+
+        foreach (var (a, b) in digraphs)
+        {
+            var (row1, col1) = FindPosition(a);
+            var (row2, col2) = FindPosition(b);
+
+            if (row1 == row2)
+            {
+                sb.Append(matrix[row1, (col1 + 1) % Size]);
+                sb.Append(matrix[row2, (col2 + 1) % Size]);
+            }
+            else if (col1 == col2)
+            {
+                sb.Append(matrix[(row1 + 1) % Size, col1]);
+                sb.Append(matrix[(row2 + 1) % Size, col2]);
+            }
+            else
+            {
+                sb.Append(matrix[row1, col2]);
+                sb.Append(matrix[row2, col1]);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public static string Decrypt(string ciphertext)
+    {
+        List<(char, char)> digraphs = new();
+        for (int i = 0; i < ciphertext.Length; i += 2)
+        {
+            digraphs.Add((ciphertext[i], ciphertext[i + 1]));
+        }
+
+        StringBuilder sb = new();
+
+        foreach (var (a, b) in digraphs)
+        {
+            var (row1, col1) = FindPosition(a);
+            var (row2, col2) = FindPosition(b);
+
+            if (row1 == row2)
+            {
+                sb.Append(matrix[row1, (col1 + Size - 1) % Size]);
+                sb.Append(matrix[row2, (col2 + Size - 1) % Size]);
+            }
+            else if (col1 == col2)
+            {
+                sb.Append(matrix[(row1 + Size - 1) % Size, col1]);
+                sb.Append(matrix[(row2 + Size - 1) % Size, col2]);
+            }
+            else
+            {
+                sb.Append(matrix[row1, col2]);
+                sb.Append(matrix[row2, col1]);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public void PrintMatrix()
+    {
+        Console.WriteLine("Playfair Matrisi:");
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+                Console.Write(matrix[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+}
+
